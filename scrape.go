2955c5a7c880c0e1b2f035a81e42e51661f0f8d0
ablades <audaris@gmail.com>
2020-02-18 15:24:12 -0800

    add realestate links

--- scrape.go
+++ scrape.go
@@ -6,24 +6,48 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
-func main() {
-	// Instantiate default collector
+func realestate() {
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("b a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		title := e.Attr("title")
-		fmt.Printf("Link found: %q -> %s\n", title, link)
-
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting")
 	})
 
-	// Before making a request print "Visiting ..."
+	c.OnHTML(".px11.darkbrown.bold", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+
+		fmt.Printf("Link found: %s -> %s \n", e.Text, link)
+	})
+
+	c.Visit("https://www.realestateabc.com/home-values/search/GA")
+}
+
+func episodeLinks() {
+	//Manages Network Collection Executes Callbacks
+	c := colly.NewCollector()
+
+	//On Request
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	//On Found HTML
+	c.OnHTML("b a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		title := e.Attr("title")
+		fmt.Printf("Link found: %q -> %s\n", title, link)
+	})
+
+	//Website to visit
 	c.Visit("https://vampirediaries.fandom.com/wiki/Season_One_(Legacies)")
+}
+
+// main() contains code adapted from example found in Colly's docs:
+// http://go-colly.org/docs/examples/basic/
+func main() {
+
+	//episodeLinks()
+
+	realestate()
+
 }
